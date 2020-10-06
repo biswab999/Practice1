c61856d7e47b9a4eb7116f95b098266eb56784d8
sony <sony@DESKTOP-F8VUMGK>
2020-10-06 14:57:46 +0530

    my second

---
+++ FirstProjectWithGIT/src/test/java/com/qspider/FirstProjectWithGIT/scripts/TC008.java
@@ -0,0 +1,12 @@
+package com.qspider.FirstProjectWithGIT.scripts;
+
+import org.testng.annotations.Test;
+//No QuickView Used
+public class TC008 extends BaseTest
+{
+	@Test(description="Verify Whether the Added Product is displayed in ODP")
+	public void testItemAddedToKart()
+	{
+		
+	}
+}
